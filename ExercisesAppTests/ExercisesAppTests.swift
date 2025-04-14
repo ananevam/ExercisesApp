788a968fd2e1c7bb2ec7bf0ae8769cea5428ba2d
Ananev Aleksandr <minoru.null@gmail.com>
2025-04-15 00:21:20 +0500

    init

---
+++ ExercisesAppTests/ExercisesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisesAppTests.swift
+//  ExercisesAppTests
+//
+//  Created by Alexander Ananyev on 15.04.2025.
+//
+
+import Testing
+@testable import ExercisesApp
+
+struct ExercisesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
