cb2178b2785cafea1af8d90b10cae0ab4da14ebf
Ananev Aleksandr <minoru.null@gmail.com>
2025-04-22 17:29:34 +0500

    add WorkoutExercise CoreData model and relationships

--- ExercisesApp/Models/CoreData/Workout.swift
+++ ExercisesApp/Models/CoreData/Workout.swift
@@ -5,8 +5,13 @@ public class Workout: NSManagedObject {
     @NSManaged public var id: UUID
     @NSManaged public var name: String
     @NSManaged public var createdAt: Date
+    @NSManaged public var exercises: NSSet?
+    var exercisesArray: [WorkoutExercise] {
+        (exercises as? Set<WorkoutExercise>)?.sorted { $0.exerciseId < $1.exerciseId } ?? []
+    }
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Workout> {
-        return NSFetchRequest<Workout>(entityName: String(describing: Workout.self))
+        let request = NSFetchRequest<Workout>(entityName: String(describing: Workout.self))
+        return request
     }
 }
---
+++ ExercisesApp/Models/CoreData/WorkoutExercise.swift
@@ -0,0 +1,12 @@
+import CoreData
+
+@objc(WorkoutExercise)
+public class WorkoutExercise: NSManagedObject {
+    @NSManaged public var id: UUID
+    @NSManaged public var exerciseId: String
+    @NSManaged public var workout: Workout
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<WorkoutExercise> {
+        return NSFetchRequest<WorkoutExercise>(entityName: String(describing: WorkoutExercise.self))
+    }
+}
